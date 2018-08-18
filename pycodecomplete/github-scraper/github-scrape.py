95bc9d8a8c191016a3cd0003fa4e68086fd7e77b
Kevin Africa <kevinafrica@gmail.com>
2018-08-17 17:17:56 -0700

    Update github-scrape.py

--- pycodecomplete/github-scraper/github-scrape.py
+++ pycodecomplete/github-scraper/github-scrape.py
@@ -1,17 +1,11 @@
+import sys
 import pymongo
-
-mongo_client = pymongo.MongoClient('mongodb://localhost:27017/')
-pycodecomplete_db = mongo_client['pycodecomplete_database']
-repo_col = pycodecomplete_db['repos']
-
-mydict = { "name": "John", "address": "Highway 37" }
-
-x = repo_col.insert_one(mydict)
 
 def mongo_collection(host='mongodb://localhost:27017/',
                      database='pycodecomplete_database',
                      collection='repos'):
-'''Return MongoDB repo collection'''
+    '''Return MongoDB repo collection'''
+
     mongo_client = pymongo.MongoClient('mongodb://localhost:27017/')
     pycodecomplete_db = mongo_client['pycodecomplete_database']
     repo_col = pycodecomplete_db['repos']
@@ -23,10 +17,15 @@ def mongo_collection(host='mongodb://localhost:27017/',
         print('Start MongoDB with: sudo service mongod start')
         raise    
 
-    return repo_col
+    return mongo_client, pycodecomplete_db, repo_col
                     
 def main(argv):
-    pass
+    try:
+        mongo_client, pycodecomplete_db, repo_col = mongo_collection()
+    except Exception:
+        sys.exit(0)
+
+    repo_col.drop()
 
 if __name__ == "__main__":
     main(sys.argv[1:])
