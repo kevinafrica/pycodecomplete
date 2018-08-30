7da444464c9dd609e4550ce117bd51841bf66992
Kevin Africa <kevinafrica@gmail.com>
2018-08-30 16:24:22 -0700

    Update rnn.py

--- pycodecomplete/ml/rnn.py
+++ pycodecomplete/ml/rnn.py
@@ -59,6 +59,7 @@ class pyCodeRNNBuilder():
         if self.model is None:
             self.build_model()
         elif self.n_gpu is not None:
+            print('Continuing training existing model')
             print('Using', self.n_gpu, 'GPUs')
             parallel_model = multi_gpu_model(model, gpus=self.n_gpu)
             parallel_model.compile(loss='categorical_crossentropy', optimizer="adam")
